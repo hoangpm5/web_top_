419a5741a784de63752d3208e94dd640e9f941dd
hoangpm5 <hoang12041992@gmail.com>
2025-11-10 19:02:58 +0700

    Update 006.py

--- 006.py
+++ 006.py
@@ -29,7 +29,7 @@ if menu == "🎤 Favorite music artist":
             ("Mang tiền về cho mẹ", "https://www.youtube.com/watch?v=UVbv-PJXm14"),
             ("Trời hôm nay nhiều mây cực!", "https://www.youtube.com/watch?v=MBaF0l-PcRY"),
             ("Hai triệu năm", "https://www.youtube.com/watch?v=LSMDNL4n0kM"),
-            ("Những bài hát về mẹ", "https://www.youtube.com/watch?v=M7gWMzzdILU&list=RDM7gWMzzdILU&start_radio=1")
+            ("Những bài hát về mẹ", "https://www.youtube.com/watch?v=M7gWMzzdILU")
         ],
         "Hà Anh Tuấn": [
             ("Tuyết rơi mùa hè", "https://www.youtube.com/watch?v=pTh3KCD7Euc"),
@@ -240,4 +240,5 @@ elif menu == "👌 Tư duy & Nội lực":
 
     st.divider()
     st.caption("Góc nhìn tham khảo nhằm tự nhận thức và rèn luyện bản thân.")    
+
 
