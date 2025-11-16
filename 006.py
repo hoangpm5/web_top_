b31fca84a18b8cdc3b9a7b9b56adda0e7fe5e83e
hoangpm5 <hoang12041992@gmail.com>
2025-11-16 16:02:53 +0700

    Update 006.py

--- 006.py
+++ 006.py
@@ -29,7 +29,7 @@ if menu == "🎤 Favorite music artist":
             ("Mang tiền về cho mẹ", "https://www.youtube.com/watch?v=UVbv-PJXm14"),
             ("Trời hôm nay nhiều mây cực!", "https://www.youtube.com/watch?v=MBaF0l-PcRY"),
             ("Hai triệu năm", "https://www.youtube.com/watch?v=LSMDNL4n0kM"),
-            ("Những bài hát về mẹ", "https://www.youtube.com/watch?v=M7gWMzzdILU")
+            ("Trung Quân Idol", "https://www.youtube.com/watch?v=7MMiwZv0HzE&list=RD7MMiwZv0HzE&index=1")
         ],
         "Hà Anh Tuấn": [
             ("Tuyết rơi mùa hè", "https://www.youtube.com/watch?v=pTh3KCD7Euc"),
@@ -240,5 +240,6 @@ elif menu == "👌 Tư duy & Nội lực":
 
     st.divider()
     st.caption("Góc nhìn tham khảo nhằm tự nhận thức và rèn luyện bản thân.")    
+
 
 
